11c13e5de2fd825b8cf35656d5119804b9e45048
liyongqiang <liyongqiang@14cells.com>
2014-02-14 10:13:50 +0800

    gvds

---
+++ xuirui/4jdkjgk.h
@@ -0,0 +1,13 @@
+//
+//  4jdkjgk.h
+//  xuirui
+//
+//  Created by a on 14-2-14.
+//  Copyright (c) 2014年 liyongqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface _jdkjgk : UIButton
+
+@end
---
+++ xuirui/4jdkjgk.m
@@ -0,0 +1,13 @@
+//
+//  4jdkjgk.m
+//  xuirui
+//
+//  Created by a on 14-2-14.
+//  Copyright (c) 2014年 liyongqiang. All rights reserved.
+//
+
+#import "4jdkjgk.h"
+
+@implementation _jdkjgk
+
+@end
